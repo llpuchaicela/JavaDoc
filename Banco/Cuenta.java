2c1295587b900ab902b320e4d52e36c0baa80457
Lady Puchaicela <73542300+llpuchaicela@users.noreply.github.com>
2021-06-02 22:12:31 -0500

    Update Cuenta.java

--- Banco/Cuenta.java
+++ Banco/Cuenta.java
@@ -1,20 +1,42 @@
 package Banco;
 
+/** 
+* package  es el paquete donde se crean las clases.
+* @author Lilibeth Puchaicela
+* @author Anthony Franco
+* @versión 1.0
+* @since 20/05/2021
+*/
+
 import java.time.LocalDate;
 
+/**
+* Se crea la clase con el nombre Cuenta la cual será 
+* que tiene atributos privados
+* Y se importa la clase LocalDate 
+*/
 public class Cuenta {
     private double saldo;
     protected String numero;
     private LocalDate fechaApertura;
 
-    public Cuenta(double saldo, String numero) {
-        setSaldo(saldo);
-        this.numero = numero;
-        fechaApertura= LocalDate.now();
-    }
+/*
+* Creamos los metodos getters de los atributos saldo y nombre
+*/
 
     public double getSaldo() {
         return saldo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+/*
+* Creamos los metodos setters de los atributos saldo y nombre
+*/
+    
+    public void setNumero(String numero) {
+        this.numero = numero;
     }
 
     public final void setSaldo(double saldo) {
@@ -25,18 +47,19 @@ public class Cuenta {
         }
 
     }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
     private boolean validarSaldoNoNegativo(double saldo){
         return saldo < 0;
     }
+
+
+
+    public Cuenta(double saldo, String numero) {
+        setSaldo(saldo);
+        this.numero = numero;
+        fechaApertura= LocalDate.now();
+    }
+
+
 
     public void depositar(double monto){
         if(monto>0){

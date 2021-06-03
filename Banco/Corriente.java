08bec664cc21345b6be4061882f48e364c6d1294
Lady Puchaicela <73542300+llpuchaicela@users.noreply.github.com>
2021-06-02 23:08:57 -0500

    Update Corriente.java

--- Banco/Corriente.java
+++ Banco/Corriente.java
@@ -1,23 +1,29 @@
+
+    /** 
+    * package Banco es el paquete donde se crean las clases.
+    * @author Lilibeth Puchaicela
+    * @author Anthony Franco
+    * @versión 1.0
+    * @since 20/05/2021
+    */
 package Banco;
 
-/** 
-* package Banco es el paquete donde se crean las clases.
-* @author Lilibeth Puchaicela
-* @author Anthony Franco
-* @versión 1.0
-* @since 20/05/2021
-*/
 
-/**
-* Se crea la clase con el nombre Corriente y sus atributos privados
-* nroMaxCheques y secuencialCh
-*/
+    /**
+    * Se crea la clase con el nombre Corriente y sus atributos privados
+    * nroMaxCheques y secuencialCh
+    * @see extends Cuenta . Hace referencia a la clase Padre
+    */
 
 public class Corriente extends Cuenta{
     private int nroMaxCheques;
     private SecuencialCheque secuencialCh;
 
-
+    /*
+    * Se crea un constructor que llama a sus atributos 
+    * @see Super refiere a los atributos de la clase padre
+    */
+    
     public Corriente(double saldo, String numero) {
         super(saldo, numero);
     }

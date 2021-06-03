929f8560e6c6d6ecec814384463f8c18258c96a6
Lady Puchaicela <73542300+llpuchaicela@users.noreply.github.com>
2021-06-02 23:04:12 -0500

    Update Ahorro.java

--- Banco/Ahorro.java
+++ Banco/Ahorro.java
@@ -1,30 +1,41 @@
+    /** 
+    * package Banco es el paquete donde se crean las clases.
+    * @author Lilibeth Puchaicela
+    * @author Anthony Franco
+    * @versión 1.0
+    * @since 20/05/2021
+    */
+
 package Banco;
 
-/** 
-* package  es el paquete donde se crean las clases.
-* @author Lilibeth Puchaicela
-* @author Anthony Franco
-* @versión 1.0
-* @since 20/05/2021
-*/
-
-/**
-* Se crea la clase con el nombre Ahorro
-* que es hija de la clase padre Cuenta
-*/
+    /**
+    * Se crea la clase con el nombre Ahorro que extiendede la clase Cuenta
+    * y su atributo privado interesAnual con tipo de dato double
+    * @see extends Cuenta . Hace referencia a la clase Padre
+    */
 
 public class Ahorro extends Cuenta{
     private double interesAnual;
 
+    /*
+    * Se crea un constructor que llama a su atributo interesAnual
+    * @see Super refiere a los atributos de la clase padre
+    */
+    
     public Ahorro(double saldo, String numero, double interesAnual) {
         super(saldo, numero);
         this.interesAnual = interesAnual;
     }
 
-    @Override
+   //Creamos los metodos getters de los atributos Numero e InteresAnual
+
+    
+    @Override 
+        
     
     /*
-    * Creamos los metodos getters de los atributos Numero e InteresAnual
+    * Metodo sobreescrito getNúmero  
+    * @see getNumero, referencia del metodo de clase cuenta
     */
     
     public String getNumero() {
@@ -34,10 +45,21 @@ public class Ahorro extends Cuenta{
     public double getInteresAnual() {
         return interesAnual;
     }
+    
+    /**
+    * @param monto: valida si el monto es valido
+    * @return monto : devuelve el valor si el monto a retirar es mayor a 0 
+    */
+    
     private boolean isMontovalido(double monto){
-        return monto >0;
+        return monto > 0;
     }
 
+    /**
+    * Metodo para retirar un monto de la cuenta
+    * @param monto: valida si el monto es valido procede con el retiro
+    */
+    
     public void retirar(double monto){
         if(isMontovalido(monto)){
             setSaldo(getSaldo()-monto);
